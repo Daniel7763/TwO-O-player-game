ddfaaf302c189669927252490bfb637f9c7e1d95
Daniel Soares <danielsoares01@outlook.com>
2023-07-03 17:55:47 -0700

    add random generate questions

--- game.rb
+++ game.rb
@@ -9,15 +9,30 @@ class Game
     @player1 = player1
     @player2 = player2
     @current_player = @player1
+
+    @q1 = "What is 1 + 1?"
+    @q2 = "What is 1 + 2?"
+    @q3 = "What is 1 + 3?"
+    @q4 = "What is 1 + 4?"
+    @q5 = "What is 1 + 5?"
+    @q6 = "What is 1 + 6?"
+
+    @question_array = [@q1,@q2,@q3,@q4,@q5,@q6]
+
+    @current_question = @q1
+
+    @a1 = "2", @a2 = "3", @a3 = "4", @a4 = "5", @a5 = "6", @a6 = "7"
+
+    @current_answer = @a1
   end
 
   def start_game
-    puts "welcome to the game"
-    puts "start the game?"
+    puts "Welcome to TwO-O-Player!"
 
     loop do
       display_turn_info
       ask_question
+        # loop
       validate_answer
       check_game_over
       switch_turns
@@ -25,29 +40,52 @@ class Game
   end
 
   def display_turn_info
-    puts "-------------------"
+    puts "------SCORE-------"
     puts "Current Turn: #{@current_player.name}"
-    puts "Score: #{@current_player.get_score}"
-    puts "-------------------"
+    puts "P1: #{@player1.get_score}/3 vs P2: #{@player2.get_score}/3"
+    puts "-------NEXT QUESTION-------"
   end
 
   def ask_question
-    puts "answer the following question"
-    #add question
-    puts "What is 5 plus 3"
+    puts "#{@current_player.name}: answer the following question"
+    puts @current_question
 
     #.chomp to take players answer
     @current_player_answer = gets.chomp
   end
 
+  #alternative method was to use an object instead of a case
+  #question = key of object & value = awnser
+  def get_expected_answer(question)
+    case question
+    when @q1
+      "2"
+    when @q2
+      "3"
+    when @q3
+      "4"
+    when @q4
+      "5"
+    when @q5
+      "6"
+    when @q6
+      "7"
+    else
+      "Q/A error"
+    end
+  end
+
   def validate_answer
-    #correct answer here
-    expected_answer = "8"
+
+    expected_answer = get_expected_answer(@current_question)
+
+    puts expected_answer
     if @current_player_answer == expected_answer
-      puts "You're correct!"
+      puts "CORRECT!"
       @current_player.increase_score
     else
-      puts "*quickly points at you without eye contact* WRONG"
+      puts "WRONG"
+      @current_player.get_score
     end
   end
 
@@ -62,6 +100,7 @@ class Game
       @current_player = @player2
     else @current_player = @player1
     end
+    @current_question = @question_array.sample
   end
 
   def end_game
--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
 require_relative 'player'
 require_relative 'game'
 
-# Create player instances
+# Create player objects
 player1 = Player.new("Player 1")
 player2 = Player.new("Player 2")
 
-# Create game instance
+# Initialize the game with player objects
 game = Game.new(player1, player2)
 
 # Start the game
